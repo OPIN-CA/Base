19a9ae0c76c794ff6f798216ca6d4d65690a6e87
Tyler Klepsch <tylerklepsch@Tylers-MacBook-Pro.local>
2019-06-06 14:25:32 -0400

    writing to style.css. created example.js file in dist

---
+++ dist/js/example.js
@@ -0,0 +1,18 @@
+(function ($, Drupal, window, document) {
+  'use strict';
+
+  // DO NOT USE! MEANT FOR EXAMPLE PURPOSES.
+  // Try to avoid having a global.js file. Only use small, js files.
+
+  // Example of Drupal behavior loaded.
+
+  Drupal.behaviors.themeJS = {
+    attach: function (context, settings) {
+      if (typeof context['location'] !== 'undefined') {// Only fire on document load.
+
+        /* code goes here */
+
+      }
+    }
+  };
+})(jQuery, Drupal, this, this.document);
